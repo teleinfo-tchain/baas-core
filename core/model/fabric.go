62b137a30e2742e65d0ac45e6c5dda8e8ecdaa81
15502688274 <xianqi470@qq.com>
2021-10-28 16:43:26 +0800

    修改完的core，fabric中增加了K8s设置等字段

--- core/model/fabric.go
+++ core/model/fabric.go
@@ -5,37 +5,86 @@ import (
 	"strings"
 )
 
-type PeerConfig struct {
-	Peercount int
-	Country   string
-	Province  string
-	Locality  string
-	Usercount int
+type PeerK8sConfig struct {
+	Cpu          string `json:"cpu"`
+	Memory       string `json:"memory"`
+	EndpointPort string `json:"endpoint_port"`
+	WorkerLabel  string `json:"worker_label"`
+	ClPort       string  `json:"cl_port"`
+	CouchdbPort  string  `json:"couchdb_port"`
+	OutputPort   string  `json:"output_port"`
+	IP           string   `json:"ip"`
+	Nodeid       string   `json:"nodeid"`
+	Org          string   `json:"org"`
+	PodName      string   `json:"pod_name"`
+	Volume       Volumes
+}
+
+type Volumes struct {
+	ArtifactPV         string `json:"artifact_pv"`
+	ArtifactStorage    string `json:"artifact_storage"`
+	DataPV             string  `json:"data_pv"`
+	DataStorage        string  `json:"data_storage"`
+	ArtifactServiceIP  string `json:"artifact_service_ip"`
+	ArtifactMountPath  string `json:"artifact_mount_path"`
+	DataServiceIP      string `json:"data_service_ip"`
+	DataMountPath      string `json:"data_mount_path"`
+}
+
+
+type PeerOrgConfig struct {
+	Peercount      int
+	Country        string
+	Province       string
+	Locality       string
+	Usercount      int
+	PeerK8sconfig  map[int]*PeerK8sConfig
+	CaK8s          CaK8sConfig
 }
 
 type OrdererConfig struct {
-	Country   string
-	Province  string
-	Locality  string
+	Cpu            string
+	Memory         string
+	EndPointPort   string
+	OutputPort     string
+	WorkerLabel    string
+	IP             string
+	Nodeid         string
+	PodName        string
+	Volume         Volumes
+}
+
+type OrdererOrgConfig struct {
+	Country        string
+	Province       string
+	Locality       string
+	OrdererK8sConfig  map[int]OrdererConfig
+}
+
+type CaK8sConfig struct {
+	Cpu            string
+	Memory         string
+	EndPointPort   string
 }
 
 type FabricChain struct {
-	ChainName   string   `json:"chainName"`
 	ChainId     int      `json:"ChainId"`     //用户帐号
 	LeagueId    int      `json:"LeagueId"`     //用户帐号
 	Consensus   string   `json:"consensus"`   //共识
 	PeersOrgs   []string `json:"peersOrgs"`   //参与组织 除了orderer
 	OrderCount  int      `json:"orderCount"`  //orderer节点个数
 	//PeerCount   map[string]int      `json:"peerCount"`   //每个组织节点个数
-	Peerconfig  map[string]*PeerConfig
-	ChannelName string   `json:"channelName"` //channel 名
-	TlsEnabled  string   `json:"tlsEnabled"`  //是否开启tls  true or false
-	KafkaCount  int       `json:"kafkaCount"`
-	Ordererconfig OrdererConfig
-	Addedpeerorg    []string `json:"addedperorg"`
-	Addedpeerconfig  map[string]PeerConfig `json:"addedpeerconfig"`
-	Addedpeer   string   `json:"addedpeer"`//该peer节点隶属的组织名
-	Addedpeercount int `json:"addedpeercount"`//该组织名下多增加几个peer节点
+	PeerOrgConfig      map[string]*PeerOrgConfig
+	ChannelName        string   `json:"channelName"` //channel 名
+	TlsEnabled         string   `json:"tlsEnabled"`  //是否开启tls  true or false
+	KafkaCount         int       `json:"kafkaCount"`
+	OrdererOrgconfig   OrdererOrgConfig
+	//Addedpeerorg       []string `json:"addedperorg"`
+	//Addedpeerconfig    map[string]*PeerOrgConfig `json:"addedpeerconfig"`
+	//Addedpeer          string   `json:"addedpeer"`//该peer节点隶属的组织名
+	//Addedpeercount     int `json:"addedpeercount"`//该组织名下多增加几个peer节点
+	//AddedOrderer       int `json:"added_orderer"`
+	//AddedOrdererConfig map[int]*OrdererConfig
 }
 
 func (f FabricChain) GetHostDomain(org string) string {
@@ -50,6 +99,15 @@ type FabricChannel struct {
 	Version        string   `json:"version"`
 	Policy         string   `json:"policy"`
 	Args           [][]byte `json:"args"`
+}
+
+
+type AddedPeerOrg struct {
+
+}
+
+type AddedNode struct {
+
 }
 
 type DeleteNode struct {
